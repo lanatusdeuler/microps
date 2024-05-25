21b6e095817835260b4c9eca9b13be7061a112b2
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 07:47:46 +0900

    Skeleton of step9

--- icmp.c
+++ icmp.c
@@ -1,9 +1,12 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "util.h"
 #include "ip.h"
 #include "icmp.h"
+
+#define ICMP_BUFSIZ IP_PAYLOAD_SIZE_MAX
 
 #define icmp_type com.type
 #define icmp_code com.code
@@ -121,6 +124,11 @@ icmp_input(const struct ip_hdr *iphdr, const uint8_t *data, size_t len, struct i
         len);
     debugdump(data, len);
     icmp_print(data, len);
+}
+
+int
+icmp_output(uint8_t type, uint8_t code, uint32_t val, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst)
+{
 }
 
 int
